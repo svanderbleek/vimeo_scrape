74246fec8196194a08b3bd146db1d8b9a45ce4ba
Sandy Vanderbleek <sandy.vanderbleek@gmail.com>
2014-02-28 02:33:19 -0800

    Use VideosPage to organize downloading

--- vimeo_scrape.py
+++ vimeo_scrape.py
@@ -11,21 +11,22 @@ class UserVideos:
     self.name = name
 
   def download(self):
-    map(self.__download_page, self.__pages())
-
-  def __download_page(self, page_url):
-    video_links = VideoLinks(page_url, ".browse_videos a")
-    urls_and_titles = zip(video_links.urls(), video_links.titles())
-    for url_and_title in urls_and_titles:
-      video = UserVideo(*url_and_title)
-      video.download()
+    for page in self.__pages(): page.download()
 
   def __pages(self):
     videos_url = self.VIDEOS_URL % {"name": self.name}
     page_urls = PageLinks(videos_url, "#pagination a").urls()
-    return set(page_urls)
+    return map(VideosPage, set(page_urls))
 
-class UserVideo:
+class VideosPage:
+  def __init__(self, url):
+    self.url = url
+
+  def download(self):
+    video_links = VideoLinks(self.url, ".browse_videos a")
+    for video in video_links.videos(): video.download()
+
+class Video:
   VIDEO_FILE = "videos/%(title)s.mp4"
 
   def __init__(self, url, title):
@@ -38,23 +39,22 @@ class UserVideo:
     video_content = requests.get(self.__download_url()).content
     video_file.write(video_content)
 
-  # Vimeo did not want to play nice
   def __download_url(self):
-    soup = Soup.make(self.url)
-    data_url = soup.select(".player")[0]['data-config-url']
+    dom = Dom.build(self.url)
+    data_url = dom.select(".player")[0]['data-config-url']
     data = requests.get(data_url).json()
     return data['request']['files']['h264']['sd']['url']
 
-class Soup:
+class Dom:
   @staticmethod
-  def make(url):
+  def build(url):
     html = requests.get(url).text
     return BeautifulSoup(html, "html5lib")
 
 class PageLinks:
-  def __init__(self, page_url, selector):
-    page_soup = Soup.make(page_url)
-    self.links = page_soup.select(selector)
+  def __init__(self, url, selector):
+    dom = Dom.build(url)
+    self.links = dom.select(selector)
 
   def urls(self):
     return map(self.__url_from_link, self.links)
@@ -65,6 +65,10 @@ class PageLinks:
 class VideoLinks(PageLinks):
   def titles(self):
     return map(self.__title_from_link, self.links)
+
+  def videos(self):
+    urls_and_titles = zip(self.urls(), self.titles())
+    return [Video(*url_and_title) for url_and_title in urls_and_titles]
 
   def __title_from_link(self, link):
     title_element = link.select(".title")[0]
